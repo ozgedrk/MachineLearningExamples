80361c6586ff8ec73f2e1cd0539fe184d892bf7f
ozgedrk <ozge.dirik.38@hotmail.com>
2024-10-24 14:55:45 +0300

    KNN Algorithm Updates

--- KNN_Algorithm.py
+++ KNN_Algorithm.py
@@ -1,7 +1,11 @@
 # sklearn : ML Library
 from sklearn.datasets import load_breast_cancer
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 # (1) Veri seti incelemesi
@@ -16,11 +20,96 @@ df["target"] = cancer.target
 X = cancer.data   # features
 y = cancer.target # target
 
-knn = KNeighborsClassifier()  # Model olusturma komsu parametresini unutma *****
-knn.fit(X , y) # fit fonskiyonu verimiz (samples + target) kullanarak KNN algoritmasini egitir
+# train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+
+# olceklendirme
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+ 
+## KNN modeli olustur ve train et
+knn = KNeighborsClassifier(n_neighbors=3)  # Model olusturma komsu parametresini unutma *****
+knn.fit(X_train , y_train) # fit fonskiyonu verimizi (samples + target) kullanarak KNN algoritmasini egitir
 
 # (4) Sonuclarin Degerlendirilmesi
 
-knn.predict(X)
+y_predict = knn.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_predict)
+print("Dogruluk:" , accuracy)
+
+conf_matrix = confusion_matrix(y_test, y_predict)
+print("confusion_matrix:" )
+print(conf_matrix)
 
 # (5) Hiperparametre Ayarlamasi
+
+"""
+        KNN: Hyperparameter = K
+        K: 1,2,3 ... ,N
+        Accuracy: %A, %B, %C ...
+"""
+
+
+accuracy_values = []
+k_values = []
+for k in range(1,21):
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    accuracy_values.append(accuracy)
+    k_values.append(k)
+
+plt.figure()
+plt.plot(k_values, accuracy_values, marker = "o", linestyle = "-")
+plt.title("K Degerine Gore Doguruluk")
+plt.xlabel("K Deferi")
+plt.ylabel("Dogruluk")
+plt.xticks(k_values)
+plt.grid(True)
+
+
+
+
+# %%
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsRegressor
+
+X = np.sort(5 * np.random.rand(40,1), axis = 0)  # uniform
+y = np.sin(X).ravel()  # target
+
+#plt.scatter(X,y)
+
+##### add noise
+y[::5] += 1 * (0.5 - np.random.rand(8))
+
+#plt.scatter(X,y)
+T = np.linspace(0, 5, 500)[:, np.newaxis]
+
+for i, weight in enumerate(["uniform", "distance"]):
+    
+    knn = KNeighborsRegressor(n_neighbors=5, weights=weight)
+    y_predict = knn.fit(X, y).predict(T)
+    
+    plt.subplot(2, 1, i + 1)
+    plt.scatter(X, y, color="green", label="data")
+    plt.plot(T, y_predict, color="blue", label="prediction")
+    plt.axis("tight")  # Here, use plt.axis instead of plt.axes
+    plt.legend()
+    plt.title(f"KNN Regressor weights = {weight}")
+
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+
+
