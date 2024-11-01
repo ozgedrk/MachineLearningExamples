20dd17c48a2a6544ff6be5791b9c4eac052cfe3f
ozgedrk <ozge.dirik.38@hotmail.com>
2024-11-01 11:44:41 +0300

    Desicion Tree Updates

--- DesicionTree.py
+++ DesicionTree.py
@@ -16,13 +16,13 @@ y = iris.target  # target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state = 42)
 
-# DT modeli olustur ve train et
+# Desicion Tree (Karar Agaci) modeli olustur ve train et
 
 tree_clf = DecisionTreeClassifier(criterion="gini", max_depth= 5, random_state = 42) # criterion="entropy"
 tree_clf.fit(X_train, y_train)
 
 
-# DT evulation test
+# Desicion Tree (Karar Agaci) evulation test
 y_predict = tree_clf.predict(X_test)
 accuracy = accuracy_score(y_test, y_predict)
 print("Iris veri seti ile egitilen DT modeli dogrulugu:", accuracy)
@@ -31,6 +31,40 @@ conf_matrix = confusion_matrix(y_test, y_predict)
 print("conf_matrix:")
 print(conf_matrix)
 
-plt.figure()
+plt.figure(figsize=(15,10))
 plot_tree(tree_clf, filled= True , feature_names= iris.feature_names , class_names= list(iris.target_names))
 plt.show()
+
+
+feature_importances = tree_clf.feature_importances_
+
+feature_names = iris.feature_names
+
+# Feature degerlerini onem ve etkisine gore siralama
+
+feature_importances_sorted = sorted(zip(feature_importances , feature_names), reverse=True)
+for importance, feature_name in feature_importances_sorted:
+    print(f"{feature_name} : {importance}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
